0cadb60274091f449514d09403eda222a25122dd
xinliangzhong <xinliangzhong@foxmail.com>
2017-03-02 20:05:53 +0800

    use rosrun image_read image_read -s to save image

--- image_read/src/image_read.cpp
+++ image_read/src/image_read.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
@@ -15,19 +16,34 @@ using namespace cv;
 
 void imageCallback(const cv_bridge::CvImage::ConstPtr &imgMsg);
 void depthCallback(const cv_bridge::CvImage::ConstPtr &depthMsg);
+void showHelp();
+
 
 string rgbFileName = "";
 string depthFileName = "";
 long rgbNum = 0;
 long depthNum = 0;
+string param;
+bool save_image = false;
+
 
 int main(int argc, char *argv[])
 {
+
+    showHelp();
     ros::init(argc, argv, "image_read");
     ros::NodeHandle nh;
 
     ros::Subscriber imgSub, depthSub;
-
+    if (argc > 1)
+    {
+        param = argv[1];
+        if(param == "-s")
+        {
+            save_image = true;
+        }
+    }
+    sleep(1);
     //subscribe the image from the Kinect
     imgSub   = nh.subscribe("/camera/rgb/image_color", 5, imageCallback);
     depthSub = nh.subscribe("/camera/depth_registered/image_raw",5,depthCallback);
@@ -46,15 +62,17 @@ void imageCallback(const cv_bridge::CvImage::ConstPtr &imgMsg)
 
     vector<int> compressionQuality;
     compressionQuality.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    //cout << "The encoding of image is: " << imgMsg->encoding << endl;
     //imshow("image_from_kinect", inImage);
     stringstream ss;
     ss << rgbNum;
     rgbFileName = "/home/m/ws/src/ros_projects/rgb/rgb_" + ss.str() + ".png";
-    imwrite(rgbFileName, inImage, compressionQuality);
-    cout << "write " << "  rgb_" << ss.str() << " suscessfully!" << endl;
+    if(save_image)
+    {
+        imwrite(rgbFileName, inImage, compressionQuality);
+        //cout << "write " << "  rgb_" << ss.str() << " suscessfully!" << endl;
+        rgbNum += 1;
+    }
     waitKey(30);
-    rgbNum += 1;
 }
 /**
  * @brief depthCallback
@@ -69,10 +87,22 @@ void depthCallback(const cv_bridge::CvImage::ConstPtr &depthMsg)
     stringstream ss;
     ss << depthNum;
     depthFileName = "/home/m/ws/src/ros_projects/depth/depth_" + ss.str() + ".pgm";
-    imwrite(depthFileName, depthImage, pgmFormat);
-
-    cout << "write " << "depth_" << ss.str() << " suscessfully!" << endl;
-    //imshow("depth_from_kinect",depthImage);
+    if(save_image)
+    {
+        imwrite(depthFileName, depthImage, pgmFormat);
+        //cout << "write " << "depth_" << ss.str() << " suscessfully!" << endl;
+        depthNum += 1;
+    }
+    //cout << "the value of save_image is :" << save_image << endl;
+    imshow("depth_from_kinect",depthImage);
     waitKey(30);
-    depthNum += 1;
+}
+
+
+void showHelp()
+{
+    cout << "***********Author: Mr.zhong*************" << endl;
+    cout << "This node is presented to get rgb and depth image from Kinect V1." << endl;
+    cout << "The image is synchronized in time, So you can use rosrun image_read image_read -s to save both image!" << endl;
+    cout << "-s " << "save image in the project directory." << endl;
 }
